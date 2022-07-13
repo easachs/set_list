d627c7d28c8c852f4b6e0fcd1e9722915ad0825a
Eli Sachs <easachs13@gmail.com>
2022-07-13 11:17:59 -0600

    Many to many

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Song do
   it {should belong_to :artist}
+  it { should have_many :playlist_songs }
+  it { should have_many(:playlists).through(:playlist_songs)}
 end
